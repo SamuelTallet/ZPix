e0b659b5edaa3d40a8772eca31086c7ac29ff1ea
Samuel Tallet <samuel.tallet@gmail.com>
2026-02-01 18:58:59 +0100

    Update the cleaning script

--- clean.cmd
+++ clean.cmd
@@ -11,6 +11,11 @@ if /i "%DELETE_HF_CACHE%"=="y" (
     rmdir /s /q "%USERPROFILE%\.cache\huggingface" >nul 2>&1
 )
 
+set /p DELETE_TRITON_CACHE="Do you want to delete Triton cache? (y/N) "
+if /i "%DELETE_TRITON_CACHE%"=="y" (
+    rmdir /s /q "%USERPROFILE%\.triton" >nul 2>&1
+)
+
 echo Cleaning now local cache and temp files...
 rmdir /s /q ".ruff_cache" >nul 2>&1
 rmdir /s /q ".venv" >nul 2>&1
