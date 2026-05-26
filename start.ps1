c972bc9ece490e671b403c06c51077c797999034
Samuel Tallet <samuel.tallet@gmail.com>
2026-05-26 20:23:42 +0200

    Upgrade cross file dialog

--- start.ps1
+++ start.ps1
@@ -92,7 +92,7 @@ if ($gpu.Vendor -eq "NVIDIA") {
         Install-Package -Id "peft" -Version "0.18.1" -Uv $Uv
         Install-Package -Id "sdnq" -Version "0.1.6" -Uv $Uv
         Install-Package -Id "platformdirs" -Version "4.9.4" -Uv $Uv
-        Install-Package -Id "crossfiledialog" -Version "1.2.0" -Uv $Uv
+        Install-Package -Id "crossfiledialog" -Version "1.3.0" -Uv $Uv
         Install-Package -Id "gradio" -Version "6.10.0" -Uv $Uv
         $optimized = $true
     }
@@ -114,7 +114,7 @@ else {
     Install-Package -Id "peft" -Version "0.18.1" -Uv $Uv
     Install-Package -Id "sdnq" -Version "0.1.6" -Uv $Uv
     Install-Package -Id "platformdirs" -Version "4.9.4" -Uv $Uv
-    Install-Package -Id "crossfiledialog" -Version "1.2.0" -Uv $Uv
+    Install-Package -Id "crossfiledialog" -Version "1.3.0" -Uv $Uv
     Install-Package -Id "gradio" -Version "6.10.0" -Uv $Uv
 }
 
